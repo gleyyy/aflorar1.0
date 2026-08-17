3e42a9693c2b97d60e810f5b35951806e9166b6b
gleyyy <gleicianejnascimento1@gmail.com>
2026-08-17 17:05:38 -0300

    Update Supabase URL and key in supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "SUA_URL";
-const SUPABASE_KEY = "SUA_ANON_KEY";
+const SUPABASE_URL = "https://atnfazrohqgsgrzuyzup.supabase.co/rest/v1/";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0bmZhenJvaHFnc2dyenV5enVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzNDkyNTgsImV4cCI6MjEwMTkyNTI1OH0.JTXEgmCIBWA23M9k62-7x4OKmBJQ7ZVtPhyeUrMeQcU";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,

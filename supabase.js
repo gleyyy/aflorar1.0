97cc4048722e4ad75f471d35328487fb20b56d23
gleyyy <gleicianejnascimento1@gmail.com>
2026-08-28 08:20:59 -0300

    Fix variable names for Supabase client initialization

--- supabase.js
+++ supabase.js
@@ -2,6 +2,6 @@ const SUPABASE_URL = "https://atnfazrohqgsgrzuyzup.supabase.co/rest/v1/";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0bmZhenJvaHFnc2dyenV5enVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzNDkyNTgsImV4cCI6MjEwMTkyNTI1OH0.JTXEgmCIBWA23M9k62-7x4OKmBJQ7ZVtPhyeUrMeQcU";
 
 const supabaseClient = window.supabase.createClient(
-  supabaseUrl,
-  supabaseKey
+  SUPABASE_URL,
+  SUPABASE_KEY
 );

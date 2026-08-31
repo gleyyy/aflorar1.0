6379bbb3f3d112a42820f4e2dd770acc6a77f28c
gleyyy <gleicianejnascimento1@gmail.com>
2026-08-31 10:58:17 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 const { createClient } = window.supabase;
 
 const supabaseClient = createClient(
-    "https://atnfazrohqgsgrzuyzup.supabase.co/rest/v1/",
+    "https://atnfazrohqgsgrzuyzup.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0bmZhenJvaHFnc2dyenV5enVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzNDkyNTgsImV4cCI6MjEwMTkyNTI1OH0.JTXEgmCIBWA23M9k62-7x4OKmBJQ7ZVtPhyeUrMeQcU"
 );
 
@@ -38,45 +38,3 @@ form.addEventListener("submit", async (e) => {
 
     window.location.href = "inicio.html";
 });
-const form =
-    document.getElementById("loginForm");
-
-form.addEventListener(
-    "submit",
-    async (e) => {
-
-        e.preventDefault();
-
-        const email =
-            document.getElementById("email").value;
-
-        const senha =
-            document.getElementById("senha").value;
-
-        const { data, error } =
-            await supabase.auth.signInWithPassword({
-
-                email,
-                password: senha
-
-            });
-
-        if(error){
-
-            alert("Login inválido");
-            return;
-
-        }
-
-        localStorage.setItem(
-            "aflorar_usuario_id",
-            data.user.id
-        );
-
-        alert("Login realizado!");
-
-        window.location.href =
-            "inicio.html";
-
-    }
-);

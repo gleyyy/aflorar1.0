716c4d82c13b026fc422e237816d93cc6dff66a9
gleyyy <gleicianejnascimento1@gmail.com>
2026-08-31 10:52:13 -0300

    Refactor login.js to use supabaseClient

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ const { createClient } = window.supabase;
 
 const supabaseClient = createClient(
     "https://atnfazrohqgsgrzuyzup.supabase.co/rest/v1/",
-    "SUA_CHAVE_ANON"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0bmZhenJvaHFnc2dyenV5enVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzNDkyNTgsImV4cCI6MjEwMTkyNTI1OH0.JTXEgmCIBWA23M9k62-7x4OKmBJQ7ZVtPhyeUrMeQcU"
 );
 
 const form = document.getElementById("loginForm");

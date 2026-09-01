23d425d41a8f4df99fd28974cac768b27f4750a6
gleyyy <gleicianejnascimento1@gmail.com>
2026-09-01 07:44:12 -0300

    Refactor login.js to remove redundant code

--- login.js
+++ login.js
@@ -1,21 +1,10 @@
-const { createClient } = window.supabase;
-
-const supabaseClient = createClient(
-    "https://atnfazrohqgsgrzuyzup.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0bmZhenJvaHFnc2dyenV5enVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzNDkyNTgsImV4cCI6MjEwMTkyNTI1OH0.JTXEgmCIBWA23M9k62-7x4OKmBJQ7ZVtPhyeUrMeQcU"
-);
-
 const form = document.getElementById("loginForm");
 
 form.addEventListener("submit", async (e) => {
-
     e.preventDefault();
 
-    const email =
-        document.getElementById("email").value;
-
-    const senha =
-        document.getElementById("senha").value;
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
 
     const { data, error } =
         await supabaseClient.auth.signInWithPassword({
@@ -35,6 +24,5 @@ form.addEventListener("submit", async (e) => {
     );
 
     alert("Login realizado!");
-
     window.location.href = "inicio.html";
 });
